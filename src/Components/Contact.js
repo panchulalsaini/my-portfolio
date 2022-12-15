95c97ed6ce009d731b3b16b9314fb6bdb6cbc2f2
panchu lal saini <sainip103@gmail.com>
2022-12-15 16:09:19 +0530

    updated my portfolio

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,25 +1,63 @@
-import React from 'react'
+import { useState } from "react";
+import React  from 'react'
 
 function Contact() {
+  const[formData,setFormData] =useState({
+    Cname :"",
+    phone :"",
+    email : "",
+    textarea:""
+  });
+  const getData =(event) =>{
+    const name = event.target.name;
+    const value = event.target.value;
+      setFormData((previous) =>{ 
+        return{ ...previous, [name]: value};
+
+        })
+        
+      
+  }
+  const printData = ()=>{
+    alert(`Hey ${formData.Cname}, Your mob. is ${formData.phone}, Your email is ${formData.email}. Thankyou for contacting us`)
+
+
+  }
   return (
     <>
     <div className='container'>
     <h1>Contact Us</h1>
 <form className='about-form'>
-<div className="fdesign">  
-   <input type="text" name="Cname"  placeholder='Enter Name'/><br/>
+<div className="fdesign"> 
+
+
+   <input type="text" name="Cname"  placeholder='Enter Name'
+    value={formData.Cname}
+     onChange={getData}
+     /><br/>
  </div>
  <div className="fdesign">
- <input type="phone" name="phone" placeholder='Enter your phone no. '/><br/>
+ <input type="phone" name="phone" placeholder='Enter your phone no.'
+   value={formData.phone}
+   onChange={getData}
+   /><br/>
     </div>
     <div className="fdesign">   
-       <input type="email" name="email" placeholder='Enter email id'/><br/>
+       <input type="email" name="email" placeholder='Enter email id'
+         value={formData.email}
+         onChange={getData}
+         /><br/>
         </div>
       <div >
-        <textarea className='textarea'>Write Some text.....</textarea>
+        <textarea className='textarea' name='textarea' 
+          value={formData.textarea}
+          onChange={getData} >Write Some text.....</textarea>
       </div>
-        <button class="but" type="submit"> Submit</button>
+        <button class="but" type="submit" onClick={printData}> Submit</button>
     </form>
+    <div>
+
+    </div>
 </div>
     
 
